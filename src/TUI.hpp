d2d23c797de63d7ec0ddfd41e77c5aa40550d1f0
Joshua Fronsee <jbfronsee@gmail.com>
2021-03-04 14:53:43 -0700

    Added TUI.hpp

---
+++ src/TUI.hpp
@@ -0,0 +1,13 @@
+#ifndef TUI_H
+#define TUI_H
+#include <string>
+
+class TUI
+{
+  private:
+
+  public:
+    void DisplayMessageAt(const std::string& message, int x, int y);
+};
+
+#endif /* TUI_H */
